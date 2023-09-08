8141173e356f1223d1ff0951b96a91fcae4a1622
Haris1245 <haris.v.photography@gmail.com>
2023-09-08 15:14:21 +0300

    fixed a typo

--- remote_dev_decision_maker.py
+++ remote_dev_decision_maker.py
@@ -3,7 +3,7 @@ wlb_is_dealbreaker = input("Remote development can lead to difficulties separati
 distractions_is_dealbreaker = input("Remote development as a digital nomad can make it hard to focus on work due to FOMO - is this a dealbreaker to you? (yes/no)").lower() == "yes"
 
 if loneliness_is_dealbreaker or wlb_is_dealbreaker or distractions_is_dealbreaker:
-    print("Don't become a demote dev")
+    print("Don't become a remote dev")
 else:
     remote_trial_time = 0
     while remote_trial_time < 6:
@@ -13,6 +13,6 @@ else:
     if decision == "yes":
         print("Become a remote dev")
     else:
-        print("Don't become a demote dev")
+        print("Don't become a remote dev")
 
     
